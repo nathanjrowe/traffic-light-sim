8af06080ba5d29cc069b596f628f0304784a3f0d
Ricardo Ponce <rponce1@unm.edu>
2024-03-23 20:19:29 -0600

    Basic UI start up. Edited the ignore file

--- src/Traffic.java
+++ src/Traffic.java
@@ -1,2 +1,23 @@
-public class Traffic {
+import javafx.application.Application;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+public class Traffic extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Pane pane = new Pane();
+
+        Scene scene = new Scene(pane, 800, 400);
+
+
+        primaryStage.setTitle("Traffic Light");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
