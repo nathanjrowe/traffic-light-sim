4b4669b7c0f6a0b0d43b39fb8ae60da131e03713
Sean Davies <seandavies@unm.edu>
2024-04-02 14:07:03 -0600

    More Testing work

--- src/Testing.java
+++ src/Testing.java
@@ -1,19 +1,30 @@
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Line;
+import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
-import org.w3c.dom.events.Event;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class Testing extends Application {
+
+    private final Boolean DEBUG = false;
+
+    private double[] polygonPoints = new double[100];
+    private List<Double> polygonTemp = new ArrayList<>();
+    private List<double[]> polygonTempArray = new ArrayList<>();
+    private int count = 0;
+
+
+    private List<double[]> allPossiblePaths = new ArrayList<>(); //Does not include Starting Paths
+    private List<double[]> startingPaths = new ArrayList<>();
 
     public static void main(String[] args) {
         launch(args);
@@ -31,30 +42,56 @@ public class Testing extends Application {
         System.out.println("Original Image Width: " + imageW + " Original Image Height: " + imageH);
 
         Pane mapPane = resizeImage(fullMap, 1200, 800);
+        root.getChildren().add(mapPane);
+
+        Pane tempPane = new Pane();
+
+        polygonTempArray.add(new double[4]);
 
         root.setOnMouseClicked(event -> {
-            double temp[] = reverseCoords(event.getX(), event.getY(), imageW, imageH);
-            System.out.println("X Position: " + temp[0] + " Y Position: " + temp[1]);
+            double[] temp = {event.getX(), event.getY()};
+            polygonTemp.add(temp[0]);
+            polygonTemp.add(temp[1]);
+            if (DEBUG) System.out.println("X Position: " + temp[0] + " Y Position: " + temp[1]);
+            int var1 = 0;
+            if (polygonTemp.size() == 4) {
+                for (Double value : polygonTemp) {
+                    polygonPoints[count] = value;
+                    polygonTempArray.get(0)[var1] = value;
+                    count++;
+                    var1++;
+                }
+                tempPane.getChildren().add(drawPossibilities(polygonTempArray));
+                polygonTempArray.clear();
+                polygonTemp.clear();
+                polygonTempArray.add(new double[4]);
+                System.out.println(DoubleArrayToString(polygonPoints));
+                var1 = 0;
+            }
         });
 
-        root.getChildren().add(mapPane);
+        root.getChildren().add(tempPane);
 
         Scene scene = new Scene(root, 1200, 800);
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Image Viewer");
+        primaryStage.setTitle("Testing");
         primaryStage.show();
     }
 
-    public Pane drawPossibilities(List<Integer[]> Path) {
-        Pane pane = new Pane();
+    public Line drawPossibilities(List<double[]> Path) {
+        Line line = new Line();
 
-        for (Integer[] singlePath : Path){
+        for (double[] singlePath : Path){
             //              Line(startX, startY, endX, endY);
-            Line line = new Line(singlePath[0], singlePath[1], singlePath[2], singlePath[3]);
-            pane.getChildren().add(line);
+            if (DEBUG) System.out.println("starX: " + singlePath[0] + " startY: " + singlePath[1] +
+                    " endX: " + singlePath[2] + " endY: " + singlePath[3]);
+            line = new Line(singlePath[0], singlePath[1], singlePath[2], singlePath[3]);
+            if (DEBUG) System.out.println("starX: " + line.getStartX() + " startY: " + line.getStartY() +
+                    " endX: " + line.getEndX() + " endY: " + line.getEndY());
         }
+        System.out.println("Attempting to draw the line");
 
-        return pane;
+        return line;
     }
 
     public double[] reverseCoords(double mouseX, double mouseY, double imageWidth, double imageHeight) {
@@ -94,4 +131,68 @@ public class Testing extends Application {
 
         return pane;
     }
+
+    private String DoubleArrayToString(double[] array){
+        String temp = "[ ";
+        int i = 0;
+        for (double value : array){
+            if (value != 0.0) {
+                if (i == 0 ){
+                    temp += "[" + value + ", ";
+                }
+                if (i > 0 && i < 3){
+                    temp += value + ", ";
+                }
+                i++;
+                if (i == 4){
+                    temp += value + "] ";
+                    i = 0;
+                }
+            }
+        }
+        temp += "]";
+        return temp;
+    }
+
+    private boolean pathConnects(double[] path1, double[] path2) {
+        //Path1 endX == Path2 startX && Path1 endY == Path2 startY
+        return path1[2] == path2[0] && path1[3] == path2[1];
+    }
+
+    private List<double[]> generateRandomPath(List<double[]> allPathsList, List<double[]> startingPathList) {
+        Random random = new Random();
+
+        List<double[]> path = new ArrayList<>();
+        List<double[]> availablePaths = new ArrayList<>(allPathsList);
+
+        double[] currentPath = startingPathList.remove(random.nextInt(startingPathList.size()));
+        path.add(currentPath);
+
+        boolean pathFinished = false;
+        while (!pathFinished) {
+            pathFinished = true;
+            List<double[]> potentialPaths = new ArrayList<>();
+            for (double[] nextPath : availablePaths) {
+//                if (pathConnects(currentPath, nextPath)) {
+//                    path.add(nextPath);
+//                    availableSegments.remove(nextPath);
+//                    currentPath = nextPath;
+//                    pathFinished = false;
+//                    break;
+//                }
+                if (pathConnects(currentPath, nextPath)) {
+                    potentialPaths.add(nextPath);
+                    pathFinished = false;
+                }
+            }
+            if (!pathFinished) {
+                double[] chosenPath = potentialPaths.get(random.nextInt(potentialPaths.size())).clone();
+                path.add(chosenPath);
+                availablePaths.remove(chosenPath);
+                currentPath = chosenPath;
+            }
+        }
+        return path;
+    }
+
 }
