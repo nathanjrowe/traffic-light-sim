57b32288339427d9d7c4b5b6d55d885d63f67f0d
michaelp15 <123104637+michaelp15@users.noreply.github.com>
2024-04-25 14:13:23 -0600

    Comments for SystemController.java

--- src/SystemController.java
+++ src/SystemController.java
@@ -3,13 +3,16 @@ import java.util.HashMap;
 
 import javafx.scene.layout.Pane;
 
-//Controller to communicate to individual light controllers
 //TODO: Receive and send data to the light controllers
+/**
+ * Controller to communicate to individual light controllers
+ */
 public class SystemController {
-    
-    //Store the coordinates of the lights for each intersection
-    //Pass to each light controller the coordinate of the lights
-    //Object[] = {String direction, int x, int y}
+    /**
+     * Store the coordinates of the lights for each intersection
+     * Pass to each light controller the coordinate of the lights
+     * Object[] = {String direction, int x, int y}
+     */
     private final HashMap<Integer, ArrayList<Object[]>> lightCoords = new HashMap<>(){{
         put(1, new ArrayList<Object[]>(){{
             add(new Object[]{"W", 256, 108});
@@ -49,24 +52,33 @@ public class SystemController {
         }});
     }};
 
-    //HashMap to store the light controllers
+    /**
+     * HashMap to store the light controllers
+     */
     private final HashMap<Integer, LightController> lightControllers = new HashMap<>();
     //Set final value for the number of intersections
     private final int INTERSECTIONS = 6;
 
-    //Constructor
+    /**
+     * Constructor
+     */
     public SystemController() {
         spawnLights();
     }
 
-    //Create the lights for each intersection
+    /**
+     * Create the lights for each intersection
+     */
     private void spawnLights() {
         for (int i = 1; i <= INTERSECTIONS; i++) {
             lightControllers.put(i, new LightController(lightCoords.get(i)));
         }
     }
 
-    //Add lights to the scene
+    /**
+     * Add lights to the scene
+     * @param root
+     */
     public void addLights(Pane root) {
         if (root != null) {
             for (LightController lightController : lightControllers.values()) {
