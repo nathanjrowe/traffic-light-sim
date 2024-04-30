00a36173613257d918581eaff456caa80950f931
Christopher Jarek <cjarek@unm.edu>
2024-04-30 08:14:30 +0000

    Annotations

--- src/TrafficLight.java
+++ src/TrafficLight.java
@@ -4,7 +4,7 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Rectangle;//this import is unused...
 
 
 /**
