ba9410352dac2ab217ea275cbd0bddff97cc0fa5
Sean Davies <seandavies@unm.edu>
2024-05-02 13:16:53 -0600

    Clean up the Traffic file

--- src/Traffic.java
+++ src/Traffic.java
@@ -1,17 +1,10 @@
 import javafx.application.Application;
-import javafx.application.Preloader;
-import javafx.scene.control.Button;//unused import
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;//unused import
-import javafx.scene.shape.Rectangle;//unused import
 import javafx.stage.Stage;
-import javafx.scene.Scene;
 
 public class Traffic extends Application {
 
     public static void main(String[] args) {
-        //System.setProperty("javafx.preloader", SplashScreen.class.getName());
         launch(args);
     }
 
@@ -24,9 +17,6 @@ public class Traffic extends Application {
         Pane pane = new Pane();
 
         TrafficScene trafficScene = new TrafficScene();
-
-        /*Scene scene = new Scene(pane, 800, 400);
-        scene.setFill(Color.GRAY);*/
 
         primaryStage.setTitle("Traffic Light");
         primaryStage.setScene(trafficScene.Traffic());
